9756f62b8df75131157f8e0e7a60d11f91db5ed2
tifernandes <tiagofer1998@gmail.com>
2023-10-08 16:22:13 -0300

    update

--- pages/contato.js
+++ pages/contato.js
@@ -8,6 +8,7 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
 import Head from 'next/head'
+import Image from 'next/image';
 
 const TextMaskCustom = React.forwardRef(function TextMaskCustom(props, ref) {
   const { onChange, ...other } = props;
@@ -28,67 +29,75 @@ const Contato = () => {
       <Head>
           <title>The King of Boxes - Contato</title>
       </Head>
-      <div className={styles.infoContato}>
-        <div className={styles.local}>
-          <h1>ONDE ESTAMOS</h1>
-          <br/>
-          <p>Escritório: Rua Leonídio Porcionato, 58<br/>
-          Bairro Parque Tomas Saraiva<br/>
-          São Paulo – SP CEP: 03280-040<br/>
-          E-mail: contato@thekingofboxes.com.br<br/>
-          WhatsApp: (11) 2717-3954 <br/>
-          Horário de Funcionamento:<br/>
-          -De segunda-feira a quinta-feira 07:00-17:00<br/>
-          -De sexta-feira 07:00-16:00</p>
-          <br/>
-          <br/>
-          <h1>SOCIAL</h1>
-          <br/>
-          <div className={styles.socialMedia}>
-          <a href="https://wa.me/message/6FZ3YWIMM7XWF1" target="_blank" rel="noopener noreferrer">
-            <p><WhatsAppIcon /></p>
-          </a>
-          <a href="https://www.instagram.com/thekingofboxes_/" target="_blank" rel="noopener noreferrer">
-            <p><InstagramIcon /></p>
-          </a>
-          </div>
-        </div>
-        {/* <div className={styles.formulario}>
-          <h1>CONTATO</h1>
-          <Box
-            component="form"
-            sx={{
-              '& .MuiTextField-root': { mt: 1, mb: 1, width: '100%' },
-            }}
-            noValidate
-            autoComplete="off"
-          >
-            <div className={styles.fieldsForm}>
-              <TextField label="Nome" id="outlined-size-normal" />
-              <TextField label="E-Mail" id="outlined-size-normal" />
-              <TextField
-                label="Celular"
-                name="textmask"
-                id="formatted-text-mask-input"
-                InputProps={{
-                  inputComponent: TextMaskCustom,
-                }}
-              />
-              <TextField
-                id="outlined-multiline-static"
-                label="Mensagem"
-                multiline
-                rows={4}
-              />
-              <Button variant="contained" endIcon={<SendIcon />}>
-                Enviar
-              </Button>
-            </div>
-          </Box>
-        </div> */}
+      <div className={styles.bannerContainer}>
+        <Image className={styles.banner} src="/contato.png" fill quality={100}/>
+        <Image className={styles.bannerMB} src="/contatoMB.png" fill quality={100}/>
       </div>
-      <div className={styles.maps}>
-        <iframe loading="lazy" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.5734045801246!2d-46.543407125294856!3d-23.583760062414353!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5da499b22fad%3A0x8ffdc054e8c7f334!2sR.%20Leon%C3%ADdio%20Porcionato%2C%2058%20-%20Parque%20Tomas%20Saraiva%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2003280-040%2C%20Brasil!5e0!3m2!1spt-BR!2sus!4v1682179051267!5m2!1spt-BR!2sus" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+      <div className={styles.subContainer}>
+        <div className={styles.infoContato}>
+            <div>
+              <h1>ONDE ESTAMOS</h1>
+              <br/>
+              <p>Escritório: Rua Leonídio Porcionato, 58<br/>
+              Bairro Parque Tomas Saraiva<br/>
+              São Paulo – SP CEP: 03280-040<br/>
+              E-mail: contato@thekingofboxes.com.br<br/>
+              WhatsApp: (11) 2717-3954 <br/>
+              Horário de Funcionamento:<br/>
+              -De segunda-feira a quinta-feira 07:00-17:00<br/>
+              -De sexta-feira 07:00-16:00</p>
+              <br/>
+              <br/>
+            </div>
+            <div>
+              <h1>SOCIAL</h1>
+              <br/>
+              <div className={styles.socialMedia}>
+              <a href="https://wa.me/message/6FZ3YWIMM7XWF1" target="_blank" rel="noopener noreferrer">
+                <p><WhatsAppIcon /></p>
+              </a>
+              <a href="https://www.instagram.com/thekingofboxes_/" target="_blank" rel="noopener noreferrer">
+                <p><InstagramIcon /></p>
+              </a>
+            </div>
+          </div>
+          {/* <div className={styles.formulario}>
+            <h1>CONTATO</h1>
+            <Box
+              component="form"
+              sx={{
+                '& .MuiTextField-root': { mt: 1, mb: 1, width: '100%' },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <div className={styles.fieldsForm}>
+                <TextField label="Nome" id="outlined-size-normal" />
+                <TextField label="E-Mail" id="outlined-size-normal" />
+                <TextField
+                  label="Celular"
+                  name="textmask"
+                  id="formatted-text-mask-input"
+                  InputProps={{
+                    inputComponent: TextMaskCustom,
+                  }}
+                />
+                <TextField
+                  id="outlined-multiline-static"
+                  label="Mensagem"
+                  multiline
+                  rows={4}
+                />
+                <Button variant="contained" endIcon={<SendIcon />}>
+                  Enviar
+                </Button>
+              </div>
+            </Box>
+          </div> */}
+        </div>
+        <div className={styles.maps}>
+          <iframe loading="lazy" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.5734045801246!2d-46.543407125294856!3d-23.583760062414353!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5da499b22fad%3A0x8ffdc054e8c7f334!2sR.%20Leon%C3%ADdio%20Porcionato%2C%2058%20-%20Parque%20Tomas%20Saraiva%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2003280-040%2C%20Brasil!5e0!3m2!1spt-BR!2sus!4v1682179051267!5m2!1spt-BR!2sus" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+        </div>
       </div>
     </div>
   );
--- pages/sobrenos.js
+++ pages/sobrenos.js
@@ -1,43 +1,76 @@
 import React from 'react';
-import styles from '../styles/index.module.css';
-import Image from 'next/image';
-import Produtos from '../components/Produtos';
+import styles from '../styles/sobrenos.module.css';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { IMaskInput } from 'react-imask';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
 import Head from 'next/head'
-import Link from 'next/link'
+import Image from 'next/image';
 
 const SobreNos = () => {
   return (
     <div className={styles.container}>
       <Head>
-          <title>The King of Boxes - Início</title>
+          <title>The King of Boxes - Sobre Nós</title>
       </Head>
-      <div className={styles.slide}>
-        <div className={styles.slide1}>
-          <div className={styles.slideTxt}>
-            <h1>Produtos inovadores e de excelente qualidade</h1>
-            <h3>Somos uma empresa 100% nacional, fabricante e especializada em expositores para colecionáveis</h3>
-          </div>
-        </div>
-        {/* <Image className={styles.logoImg} src="/slide2.jpg" fill/> */}
+      <div className={styles.bannerContainer}>
+        <Image className={styles.banner} src="/sobrenos.png" fill quality={100}/>
+        <Image className={styles.bannerMB} src="/sorbenosMB.png" fill quality={100}/>
       </div>
       <div className={styles.subContainer}>
-        <div className={styles.categorias}>
-          <div className={styles.ctHeader}>
-            <h2>LINHA DE PRODUTOS</h2>
-          </div>
-          <div className={styles.cts}>
-            <Link href={'/produtos?ct=Acessorios'}>
-              <Image className={styles.logoImg} src="/acessorios.png" width={328} height={314}/>
-            </Link>
-            <Link href={'/produtos?ct=Expositor'}>
-              <Image className={styles.logoImg} src="/expositores.png" width={328} height={314}/>
-            </Link>
-            {/* <Link href={'/produtos?ct=organizacao'}>
-              <Image className={styles.logoImg} src="/organizacao.png" width={328} height={314}/>
-            </Link> */}
-          </div>
+        <div className={styles.infoContato}>
+          <h1>A <strong>The King of Boxes</strong> é uma empresa 100% nacional fabricante e especializada em expositores para colecionáveis e caixas para organização.</h1>
+
+          <p>Nós produzimos produtos para colecionadores exigentes. Tais produtos são cuidadosamente projetados e fabricados, além de serem feitos com materiais de altíssima qualidade.</p>
+
+          <p>Temos nossa própria ferramentaria e injeção de plásticos para a fabricação dos nossos produtos, onde podemos acompanhar cada passo desde a produção até a sua entrega. Contamos com mais de 40 anos de Know-How nestes seguimentos.</p>
+
+          <p>Nossos produtos são:</p>
+
+          <ul>
+              <li>Expositores/cases para Action Figures</li>
+              <li>Expositores/cases para Funko pop</li>
+              <li>Expositores/cases para miniaturas de carros escala 1:64</li>
+              <li>Expositores/cases para miniaturas de carros escala 1:43</li>
+              <li>Expositores/cases para miniaturas de carros escala 1:32</li>
+              <li>Expositores/cases para miniaturas de carros escala 1:24</li>
+              <li>Expositores/cases para miniaturas de carros escala 1:18</li>
+              <li>Expositores/cases para Dioramas e cenários</li>
+              <li>Acessórios para dioramas;</li>
+              <li>Maletas para o colecionismo e organização</li>
+              <li>Caixas plásticas</li>
+              <li>Entre outros</li>
+          </ul>
+
+          <p>Nosso objetivo é agregar no mundo do colecionismo e fazer com que o mesmo cresça cada vez mais.</p>
+
+          <p>Estamos abertos para novas parcerias. Nós trabalhamos somente com vendas no atacado.</p>
+
+          <p>Visite também:</p>
+
+          <p>Instagram: <a href="https://www.instagram.com/thekingofboxes_">@thekingofboxes_</a></p>
+
+          <p>Continue nos acompanhando para saber sempre das novidades, novos produtos, parcerias e mais.</p>
+
+          <p>Mantenha as suas coleções sempre organizadas e protegidas com a <strong>The King of Boxes</strong>.</p>
+
+          <p><strong>De colecionador para colecionador!</strong></p>
+
+          <p><strong>Missão:</strong> Executar nossos serviços com excelência, de forma a atender as expectativas e gerando satisfação dos nossos clientes.</p>
+
+          <p><strong>Visão:</strong> Ser referência em qualidade e comprometimento para nossos clientes e fornecedores.</p>
+
+          <p><strong>Valores:</strong></p>
+
+          <ul>
+              <li>Respeito às pessoas;</li>
+              <li>Confiança e qualidade no serviço prestado;</li>
+              <li>Integridade comercial</li>
+          </ul>
         </div>
-        <Produtos />
       </div>
     </div>
   );
